1920183805e78461febda8421acff520213301d9
RohitM4926 <rohitmangale4926@gmail.com>
2023-12-31 15:03:43 +0530

    react updated

---
+++ React_Practice/helloworld/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Welcome(){
+//     return(
+//         <h1>Welcome Rohit Mangale</h1>
+//     )
+// }
+
+export const Welcome =() => <h1>Hello Rohit Sanjay Mangale</h1>
+
+//export default Welcome;
